254c1f6581bbc536e2d7c1ee5ad18550343b461f
Alwin-2006 <champindian404@gmail.com>
2025-05-27 19:22:59 +0100

    another commit

--- GLUG/src/App.tsx
+++ GLUG/src/App.tsx
@@ -2,12 +2,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './Header/navbar'
+import { BrowserRouter as Router,Route,Routes, Link } from "react-router";
+
 
 function App() {
+  
 
   return (
     <>
-     <div className='bg-yellow-300'>Hello world</div>
+    <Navbar />
+     <Link to="/">Home</Link>
+     <Link to="/about">About</Link>
+     <Link to="/contact">Contact</Link>
     </>
   )
 }
