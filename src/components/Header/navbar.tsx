4cf28ddc4bd07b04449ac971645c0ee0fded79fe
Alwin-2006 <champindian404@gmail.com>
2025-05-28 14:21:54 +0100

    Made Recommended softwares page, only footer is left to be included in it

--- src/components/Header/navbar.tsx
+++ src/components/Header/navbar.tsx
@@ -2,7 +2,7 @@ import { Link, redirect } from "react-router-dom";
 import logo from "../../assets/navbar-logo.svg";
 function Navbar() {
   return (
-    <nav className="bg-black flex justify-between items-center px-8 py-1">
+    <nav className="bg-black font-primary w-full flex justify-between items-center px-8 py-1">
       <Link
         to="/"
         className="text-white text-3xl font-bold flex justify-center items-center text-wrap"
@@ -45,7 +45,7 @@ function Navbar() {
             Blog
           </Link>
         </li>
-        <li className="nav-item rounded-xl bg-[#1E1E1E] text-primary font-light py-4 px-7">
+        <li className="nav-item rounded-xl bg-[#1E1E1E] text-primary text-orange-500 font-light py-4 px-7">
           <button onClick={() => redirect("")}>Contribute</button>
         </li>
       </ul>
--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
+import Recommended_software from "./components/Recommended_software/rec";
 import { createBrowserRouter, RouterProvider } from "react-router";
 
 const router = createBrowserRouter([
   { path: "/", element: <App /> },
   { path: "/about", element: <div>About Page</div> },
   { path: "/contact", element: <div>Contact Page</div> },
+  { path: "/recommended-software", element: <Recommended_software /> },
 ]);
 
 createRoot(document.getElementById("root")!).render(
