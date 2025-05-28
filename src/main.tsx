bc09115113b630190b855dc664de725bad1469a9
Alwin-2006 <champindian404@gmail.com>
2025-05-28 15:56:47 +0100

    Recommended softwares webpage added

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { createBrowserRouter, RouterProvider } from "react-router";
+import RecommendedSoftware from "./components/Recommended_software/rec.tsx";
 
 const router = createBrowserRouter([
   { path: "/", element: <App /> },
   { path: "/about", element: <div>About Page</div> },
   { path: "/contact", element: <div>Contact Page</div> },
+  { path: "/recommended-software", element: <RecommendedSoftware /> },
 ]);
 
 createRoot(document.getElementById("root")!).render(
