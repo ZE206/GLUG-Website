a3e7d7cca2f96329d9f66215cf06739914ad6a6e
Rahan-M <rahan01729@gmail.com>
2025-05-29 07:58:21 +0530

    Created main page

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,41 @@ import Navbar from "./components/Header/navbar";
 
 function App() {
   return (
-    <div>
+    <div className="w-full bg-[#1e1e1e]">
       <Navbar />
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
-      <Link to="/contact">Contact</Link>
+      <div className="h-[89px]"></div>
+      <div className="landingBox relative w-full h-auto">
+        <img src="src/assets/Landing-block.png" alt="landing_box" className="bg-[#1e1e1e] w-full h-auto"/>
+        <img src="src/assets/the_penguins_with_bg.png" alt="GLUG_logo" className="absolute h-[83vh] right-[8rem] top-[6.5rem]" />
+      </div>
+      <div className="h-[10rem] "></div>
+      <div className="servicesBlock relative">
+        <img src="src/assets/services_block.png" alt="services-block" className="w-full h-auto" />
+        <Link to="/projects">
+          <button className="bg-transparent backdrop-opacity-10 absolute top-[25.5rem] left-[25rem] h-[4.5rem] w-[15rem] cursor-pointer"></button>
+        </Link>
+      </div>
+      <div className="h-[89px] "></div>
+      <div className="ctaBlock relative">
+        <img src="src/assets/CTA_block.png" alt="CTA Block" className="w-full"/>
+        <Link to="/linux-guide">
+          <button className="bg-transparent backdrop-opacity-10 absolute top-[22.4rem] left-[13rem] h-[6rem] w-[15.7rem] cursor-pointer"></button>
+        </Link>
+      </div>
+      <div className="h-[89px] "></div>
+      <div className="resourcesBlock relative">
+        <img src="src/assets/Resources-block.png" alt="Resources Block" className="w-full"/>
+        <Link to="/linux-guide">
+          <button className="bg-transparent backdrop-opacity-10 absolute top-[24rem] left-[24rem] h-[3.3rem] w-[13rem] cursor-pointer"></button>
+        </Link>
+        <Link to="/tutorials">
+          <button className="bg-transparent backdrop-opacity-10 absolute top-[22.5rem] left-[53rem] h-[3.3rem] w-[13rem] cursor-pointer"></button>
+        </Link>
+        <Link to="/recommended">
+          <button className="bg-transparent backdrop-opacity-10 absolute top-[22rem] left-[75rem] h-[3.3rem] w-[12.5rem] cursor-pointer"></button>
+        </Link>
+      </div>
+      <div className="h-[89px] "></div>
     </div>
   );
 }
