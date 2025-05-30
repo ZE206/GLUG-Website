8dc65862028f460d443e6bd6293d1a084f679e87
Rahan-M <rahan01729@gmail.com>
2025-05-30 13:09:23 +0530

    Created testimonials bar

---
+++ src/components/Testimony.tsx
@@ -0,0 +1,62 @@
+import React from "react";
+import { motion, AnimatePresence } from "framer-motion";
+
+interface TestimonyProps {
+  quote: string;
+  author: string;
+  total: number;
+  currNo: number;
+  onNext: ()=>void;
+  onPrev: ()=>void;
+}
+
+const Testimony: React.FC<TestimonyProps> = ({ quote, author, total, currNo, onNext, onPrev }) => {
+  return (
+    <div className="testimonialsBlock flex justify-center">
+      <div className="bg-[#191a23] rounded-[3rem] p-10 mx-60 text-white w-full overflow-hidden">
+        <AnimatePresence mode="wait">
+          <motion.div
+            key={currNo} // Key is important to trigger exit/enter
+            initial={{ opacity: 0, x: 50 }}
+            animate={{ opacity: 1, x: 0 }}
+            exit={{ opacity: 0, x: -50 }}
+            transition={{ duration: 0.4 }}
+          >
+            <div className="quote">
+              {quote}
+              <br />
+              <br />
+              <br />
+            </div>
+            <div className="author justify-self-end">~{author}</div>
+          </motion.div>
+        </AnimatePresence>
+
+        <div className="arrowBar flex items-center gap-[25vw] mt-[1rem] px-10">
+          {/* {currNo==0 && <img src="src/assets/dullArrow.png" alt="left arrow" className="cursor-pointer" />} */}
+          {/* {currNo!=0 && <img src="src/assets/Arrow.png" alt="left arrow" className="cursor-pointer rotate-180" onClick={onPrev}/>} */}
+          <img src="src/assets/Arrow.png" alt="left arrow" className="cursor-pointer rotate-180" onClick={onPrev}/>
+          <div className="starBar flex w-full">
+            {Array.from({ length: currNo }, (_, i) => (
+              <img src="src/assets/WhiteStar.png" alt="star" key={i} className="mr-[1rem] cursor-pointer" />
+            ))}
+            <img src="src/assets/BrightStar.png" alt="star" key={currNo} className="mr-[1rem] cursor-pointer" />
+            {Array.from({ length: total - currNo - 1 }, (_, i) => (
+              <img
+                src="src/assets/WhiteStar.png"
+                alt="star"
+                key={`star-${i + currNo + 1}`}
+                className="mr-[1rem] cursor-pointer"
+              />
+            ))}
+          </div>
+          {/* {currNo==total-1 && <img src="src/assets/dullArrow.png" alt="right arrow" className="cursor-pointer rotate-180" />} */}
+          {/* {currNo!=total-1 && <img src="src/assets/Arrow.png" alt="right arrow" className="cursor-pointer" onClick={onNext}/>} */}
+          <img src="src/assets/Arrow.png" alt="right arrow" className="cursor-pointer" onClick={onNext}/>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Testimony;
---
+++ src/components/User_Testimonials.tsx
@@ -0,0 +1,43 @@
+import Testimony from "./Testimony";
+import { useState } from "react";
+const UserTestimonials=()=>{
+    const testimonials=[
+        {
+          quote:"After watching the movie The Fifth Estate back in 2014, I got interested in things related to hacking and read a book on cybersecurity. Being the edgy teenager I was, I dualbooted Kali Linux on my parents’s laptop and tried to crack my own phone hotspot password using aircrack-ng. I couldn’t do it, but the whole process was too fun. This was my first exposure to anything FOSS. Although my point of entry into FOSS was through my interest in information safety and government surveillance, my interests quickly shifted from TailsOS and TOR to the Android Open-Source Project and LineageOS after reading about how much telemetry Google gets from our phones. Bricking my dad’s 50k rupee phone while trying to root it and degoogle it was definitely an unforgettable experience. I’m pretty sure my dad degoogled me that day. College was the time I started distro-hopping. I heard of github for the first time in my life in first year of college. ABSOLUTE GAMECHANGER. Sooo many programs!!! Anyways Idk how to conclude this. I use arch btw",
+          author:"Thanvir Diouf"
+        },
+        {
+          quote:"I have been a GIMP fanboy since 2018 with GIMP 2.10, always loved it. It helped me with a lot of stuff and I got used to its weird quirky stuff. All this up until last year when I was forced to install Photoshop against my will 🥺 for Tatva. But I immediately noticed that Photoshop was way better when it came to features, so I stuck with it. Never uninstalled GIMP though. It almost felt like I was cheating with a girlfriend that I never had.\nBut last week when I decided to give GIMP 3 a piece of my love 💖, I was amazed at how good it was. It felt like a whole new thing , yet gave me the same welcoming env as before. So I left that dumb Photoshop in the dark corner of my trash folder and started fresh with GIMP 3 again and I don't regret it till this day..",
+          author:"Thiruchalvan"
+        },
+        {
+          quote:"You'll learn more CS using Linux in a non CS branch than what you'll learn in CS using windows ",
+          author:"Vysakh Premkumar"
+        },
+        {
+          quote:"Linux be Good",
+          author:"Fahad Ali Habeeb Pannikaveetil Thirunelli"
+        },
+        {
+          quote:"fr",
+          author:"Emir Ziyan"
+        },
+      ]
+    
+      const [currNo, setCurrNo]=useState(0);
+      const total=testimonials.length>5? 5:testimonials.length;
+      const rightArrowClick=()=>{
+        setCurrNo((currNo+1)%total);
+      }
+    
+      const leftArrowClick=()=>{
+        if(currNo==0) setCurrNo(total-1);
+        else setCurrNo(currNo-1);
+    
+      }
+    return(
+        currNo < testimonials.length && currNo>=0 && <Testimony quote={testimonials[currNo].quote} author={testimonials[currNo].author} total={total} currNo={currNo} onNext={rightArrowClick} onPrev={leftArrowClick}/>
+    )
+}
+
+export default UserTestimonials;
