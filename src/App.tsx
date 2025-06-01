772f24c4d4b0fbaec3a6b2941a3a2d8128a81e0c
Rahan-M <rahan01729@gmail.com>
2025-06-01 09:52:26 +0530

    Replaced relative-absolute with flex

--- src/App.tsx
+++ src/App.tsx
@@ -9,14 +9,14 @@ function App() {  return (
     <div className="w-full bg-[#121212]">
       {/* <div className="h-[10rem]"></div> */}
       <div className="landingBox relative w-full h-auto">
-        <div className="title text-white text-7xl font-primary text-center font-semibold">May The Foss Be With You</div>
+        <div className="text-white text-7xl font-primary text-center font-semibold">May The Foss Be With You</div>
         <div className="body flex justify-between">
           <div className="msg p-20 pl-52 flex flex-col items-start">
             <div className="subtitle text-white text-7xl/snug font-semibold p-4 ">
               <h1>Welcome to<br/> Glug NITC!</h1>
             </div>
-            <div className="subbody mt-2 text-2xl font-primary text-[rgba(170,170,170,0.666)] ml-4">
-              We’re the GNU/Linux Users Group of NIT Calicut.<br/> From kernel to code, we dive deep into the world of<br/> FOSS, create impactful projects, and push the<br/> boundaries of what’s possible with linux and <br/ >beyond.
+            <div className="subbody mt-2 text-2xl font-primary text-[rgba(170,170,170,0.666)] ml-4 w-[30vw]">
+              We’re the GNU/Linux Users Group of NIT Calicut. From kernel to code, we dive deep into the world of FOSS, create impactful projects, and push the boundaries of what’s possible with linux and beyond.
             </div>
           </div>
           <div className="imgContainer relative hidden md:block">
@@ -26,19 +26,19 @@ function App() {  return (
           </div>
         </div>
       </div>
-      <div className="h-[10rem] "></div>
 
-      <div className="servicesBlock relative">
-        <div className="exploreTitle bg-custom_orange h-[6vh] w-[7vw] rounded-2xl absolute left-[10rem] flex items-center justify-center text-2xl">Explore
+      <div className="servicesBlock flex flex-col items-center p-10 gap-[5rem]">
+      <div className="exploreTitle self-start bg-custom_orange h-[7vh] w-[18vw] rounded-sm left-[10rem] flex items-center justify-center text-3xl font-semibold  mb-5 ">
+          Explore
         </div>
-        <div className="mainServicesBlock bg-custom_orange h-[40vh] w-[50vw] rounded-2xl absolute left-[25rem] top-[5rem] flex justify-between p-10 text-2xl">
+        <div className="mainServicesBlock bg-custom_orange h-[40vh] w-[50vw] rounded-2xl flex justify-between p-10 text-2xl">
           <div className="textBox flex flex-col justify-between p-10">
             <div className="title inset-shadow-sm">
               <div className="openSource">
-                <div className="line1 bg-white h-[4vh] p-2 rounded-md w-[10vw] flex justify-center items-center ">
+                <div className="line1 bg-white h-[4vh] p-2 rounded-md w-[15vw] flex justify-center items-center ">
                   Open Source
                 </div>
-                <div className="line2 bg-white rounded-md h-[4vh] w-[7vw] p-2 flex justify-center items-center">
+                <div className="line2 bg-white rounded-md h-[4vh] w-[11vw] p-2 flex justify-center items-center">
                   Projects
                 </div>
               </div>
@@ -54,25 +54,23 @@ function App() {  return (
         </div>
       </div>
 
-      <div className="h-[40rem] "></div>
-
-      <div className="ctaBlock relative">
-        <div className="greyBox absolute left-[20rem] bg-gray-200 rounded-3xl h-[40vh] w-[60vw] p-10 flex">
+      <div className="ctaBlock flex justify-center mt-[10rem]">
+        <div className="greyBox left-[20rem] bg-gray-200 rounded-3xl h-[40vh] w-[60vw] p-10 flex justify-around">
           <div className="textBox flex flex-col justify-between ">
-            <div className="textContent text-3xl">
+            <div className="textContent text-2xl">
             Tired of your computer begging for updates<br/>every five minutes and crashing like it’s 1999?<br/>Maybe it’s time to try Linux—because wasting<br/>your life waiting on Windows is so 2000s
             </div>
             <div className="installLinuxButton cursor-pointer bg-custom_blue h-[7vh] w-[10vw] mb-2 mr-3 rounded-2xl flex justify-center items-center text-white text-2xl font-primary">
               Install Linux
             </div>
           </div>
-          <div className="imageContainer absolute left-[35vw]">
+          <div className="imageContainer">
             <img src="src/assets/home/weird_bob.png" alt="Hey" />
           </div>
         </div>
       </div>
 
-      <div className="h-[30rem] "></div>
+      <div className="h-[10rem] "></div>
 
       <div className="resourcesBlock flex flex-col p-15 items-center">
         <div className="exploreTitle self-start bg-custom_orange h-[7vh] w-[18vw] rounded-sm left-[10rem] flex items-center justify-center text-3xl font-semibold  mb-5 ">
@@ -80,7 +78,7 @@ function App() {  return (
         </div>
         <div className="blueBox bg-custom_blue rounded-3xl h-[25vh] w-[60vw] p-10 flex justify-between">
           <div className="textbox1 flex flex-col justify-center text-left">
-            <div className="title mb-2 text-white font-primary text-2xl">
+            <div className="mb-2 text-white font-primary text-2xl">
               How to install Linux (With guides)
             </div>
             <Link to="/linux-guide">
@@ -92,7 +90,7 @@ function App() {  return (
           </div>
           <div className="whiteLine bg-white w-[2px]"></div>
           <div className="textbox2 flex flex-col justify-center text-left">
-            <div className="title mb-2 text-white font-primary text-2xl">
+            <div className="mb-2 text-white font-primary text-2xl">
               Beginner Tutorials
             </div>
             <Link to="/beginner-tutorials">
@@ -104,7 +102,7 @@ function App() {  return (
           </div>
           <div className="whiteLine bg-white w-[2px]"></div>
           <div className="textbox3 flex flex-col justify-center text-left">
-            <div className="title mb-2 text-white font-primary text-2xl">
+            <div className="mb-2 text-white font-primary text-2xl">
               Recommended Softwares
             </div>
             <Link to="/recommended-software">
