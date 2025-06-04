f47e6da4e7b73b717d60e19e5a67fac7a44b3520
Alwin-2006 <champindian404@gmail.com>
2025-06-04 12:32:21 +0100

    optimised recommended software and beginenr tut for mobile devices, also removed footer for mobile

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,9 +3,9 @@ import { Link } from "react-router";
 const Footer=()=>{
     return (
         <>
-        <div className=" container text-xl rounded-2xl  gap-30 flex  flex-col justify-between relative w-full  mx-auto px-10 py-6 text-white items-center ">
+        <div className=" container text-xl hidden rounded-2xl  gap-30 md:flex md:flex-col justify-between mx-auto px-10 py-6 text-white items-center ">
             <div className="flex flex-row justify-between w-full items-center">
-            <div className="flex gap-4 items-center text-3xl font-bold"><img src="src/assets/pengu_whitebg.png" alt="Footer" className="h-30" />GLUG NITC</div>
+            <div className="flex gap-4 items-center text-xl md:text-3xl font-bold"><img src="src/assets/pengu_whitebg.png" alt="Footer" className="h-30" />GLUG NITC</div>
             <div className="flex justify-between gap-20">
             <Link to="/" className="underline">
                 Home
